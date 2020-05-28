ad27c374f85113ac710b19179ca1c40448e2ecfb
himchanPark <ting1099@gmail.com>
2020-05-28 16:36:03 +0900

    메모 목록 구현

---
+++ HimMemo/MemoListTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MemoListTableViewController.swift
+//  HimMemo
+//
+//  Created by Himchan Park on 2020/05/28.
+//  Copyright © 2020 Himchan Park. All rights reserved.
+//
+
+import UIKit
+
+class MemoListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return Memo.dummyMemoList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let target = Memo.dummyMemoList[indexPath.row]
+        cell.textLabel?.text = target.content
+        cell.detailTextLabel?.text = target.insertDate.description
+        return cell
+    }
+
+}
